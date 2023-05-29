23f8641c9d4f866e0239e148cbab47d9cd47a39a
doron sacha <81801956+doronsacha@users.noreply.github.com>
2023-05-29 18:18:27 +0300

    Create method_naming_example.cpp

---
+++ cpp/method_naming_example.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// Bad code
+void a() {
+    // Logic for method a
+}
+
+void xyz() {
+    // Logic for method xyz
+}
+
+// Good code
+void calculateAverage() {
+    // Logic for calculating an average
+}
+
+void processData() {
+    // Logic for data processing
+}
