f1f816b0a7c379e6a2151c0e2ae9264df0271534
doron sacha <81801956+doronsacha@users.noreply.github.com>
2023-05-29 18:05:33 +0300

    Create variable_naming_example.c

---
+++ C/variable_naming_example.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Bad Code
+double calculate(double a, double b) {
+    double c = a + b;
+    double d = c * 2;
+    double e = d / 3;
+    return e;
+}
+
+// Good Code
+double calculate_average(double first_number, double second_number) {
+    double sum = first_number + second_number;
+    double doubled_sum = sum * 2;
+    double average = doubled_sum / 3;
+    return average;
+}
